2a3d22e2163ed9c08feb533df5e488741e758e3c
camxini <chamxine@gmail.com>
2025-06-16 23:17:00 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  setLanguage('zh');
+  setLanguage('en');
 };
 
 const lastUpdatedDate = {
@@ -40,6 +40,8 @@ function setLanguage(lang) {
     document.getElementById('user_text').textContent = '杨佳昕';
     document.getElementById('btn-about').textContent = '关于';
     document.getElementById('btn-projects').textContent = '项目';
+    document.getElementById('btn-skills').textContent = '技能';
+    document.getElementById('btn-download').textContent = '下载';
     document.getElementById('btn-contact').textContent = '联系';
 
     document.getElementById('about').querySelector('h2').textContent = '关于我';
@@ -48,8 +50,15 @@ function setLanguage(lang) {
     document.getElementById('projects').querySelector('h2').textContent = '项目';
     document.getElementById('projects').querySelector('p').textContent = '这里是你做过的一些项目介绍。';
 
+    document.getElementById('skills').querySelector('h2').textContent = '技能';
+    document.getElementById('skills').querySelector('p').textContent = '这里是我的技能介绍。';
+
+    document.getElementById('download').querySelector('h2').textContent = '下载';
+    document.getElementById('download').querySelector('p').textContent = '这里是我可以提供的下载链接。';
+
     document.getElementById('contact').querySelector('h2').textContent = '联系';
     document.getElementById('contact').querySelector('p').textContent = '这里是进一步的联系方式。';
+
     document.getElementById('download-cv').querySelector('a').textContent = '下载简历';
     document.getElementById('last-updated').textContent = '最后更新于 ' + lastUpdatedDate.zh;
   } else {
@@ -57,6 +66,8 @@ function setLanguage(lang) {
     document.getElementById('user_text').textContent = 'Jiaxin Yang';
     document.getElementById('btn-about').textContent = 'About';
     document.getElementById('btn-projects').textContent = 'Projects';
+    document.getElementById('btn-skills').textContent = 'Skills';
+    document.getElementById('btn-download').textContent = 'Download';
     document.getElementById('btn-contact').textContent = 'Contact';
 
     document.getElementById('about').querySelector('h2').textContent = 'About Me';
@@ -65,8 +76,15 @@ function setLanguage(lang) {
     document.getElementById('projects').querySelector('h2').textContent = 'Projects';
     document.getElementById('projects').querySelector('p').textContent = 'This is a list of some projects.';
 
+    document.getElementById('skills').querySelector('h2').textContent = 'Skills';
+    document.getElementById('skills').querySelector('p').textContent = 'This is my skills introduction.';
+
+    document.getElementById('download').querySelector('h2').textContent = 'Download';
+    document.getElementById('download').querySelector('p').textContent = 'This is further download info.';
+
     document.getElementById('contact').querySelector('h2').textContent = 'Contact';
     document.getElementById('contact').querySelector('p').textContent = 'This is further contact info.';
+
     document.getElementById('download-cv').querySelector('a').textContent = 'Download CV';
     document.getElementById('last-updated').textContent = 'Last updated at ' + lastUpdatedDate.en;
   }
